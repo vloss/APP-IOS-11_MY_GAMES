d0e90142ebb596186271d4feb6b81b0fc64a427c
vloss <viniciusvianaloss@gmail.com>
2023-02-09 09:55:52 -0300

    Criação do Projeto.

---
+++ My Games/My Games/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Games
+//
+//  Created by Vinicius Loss on 09/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
