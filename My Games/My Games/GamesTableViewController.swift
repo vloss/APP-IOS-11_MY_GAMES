c2323e152de24dc98f94ea404747968b5bb347a5
vloss <viniciusvianaloss@gmail.com>
2023-02-13 23:43:17 -0300

    Continuação de recuperação de dados para listar na TableView.

--- My Games/My Games/GamesTableViewController.swift
+++ My Games/My Games/GamesTableViewController.swift
@@ -6,19 +6,22 @@
 //
 
 import UIKit
+import CoreData
 
 class GamesTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        loadGames()
     }
 
+    func loadGames(){
+        let fetchRequest: NSFetchRequest<Game> = Game.fetchRequest()
+        let sortDescritor = NSSortDescriptor(key: "title", ascending: true)
+        
+        fetchRequest.sortDescriptors = [sortDescritor]
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
